d42dd1aeda43b15542acaad69552758d4bc50f32
Naeem Mahmud Al-Amin <naeemmahmud370@gmail.com>
2021-11-09 11:32:23 +0600

    home and search page are developed

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import './Home.css'
+import myImage from '../../../images/naeem.png'
 
 const Home = () => {
     return (
-        <div>
-            <h1>this is home</h1>
-        </div>
+        <section className="row bg-light p-5">
+            <div className="col-md-4 text-center">
+                <div>
+                    <img className="rounded-circle" src={myImage} alt="" />
+                </div>
+                <div className="pt-2">
+                    <h2>Md. Naeem Mahmud</h2>
+                    <h5>Front End Developer(React)</h5>
+                </div>
+            </div>
+            <div className="col-md-8">
+                <h2 className="fw-bold">About Myself</h2>
+                <p className="text-justify">
+                    My name is <strong>Naeem Mahmud</strong> . I am a <strong>Junior MERN-stack
+                    Web Developer</strong> . I have 1 year of strong hands-on experience developing
+                    website using MERN. Building state-of-the-art, easy to use, user-friendly
+                    websites and applications is truly a passion of mine. Amazing UI design,
+                    responsive websites, API integration, routing, user authentication, making API with Node.js, secured API with JWT token, smoother crud operation using MERN and
+                    developing full stack features using MERN Stack are the key features of the
+                    websites that I have created successfully. In addition to my knowledge
+                    base, I actively seek out new technologies and stay up-to-date on
+                    industry trends and advancements. <br /> My strength and interest include, I
+                    am a hard-working, self-motivated, and a quick learner. I can adapt to
+                    new technologies easily. I’m an empathetic person who is skilled at
+                    relating to people and understanding their needs. My short time goal is
+                    to join a company where I can utilize my skills and ability and my long-
+                    time goal is to established myself in that position where any company doesn’t want
+                    to lose me at any const. That’s All thank you.
+                </p>
+            </div>
+        </section>
     );
 };
 
--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 const Search = () => {
+
+    const [name, setName] = useState({ name: '' })
+    console.log(name)
+
+    const handleBlur = (e) => {
+        const newName = { ...name };
+        newName[e.target.name] = e.target.value;
+        setName(newName)
+    }
+
     return (
-        <div>
-            <h1>this is search</h1>
-        </div>
+        <section className="bg-light p-3">
+           {name.name?  <h1>{name.name}</h1>: <h1>Hello World</h1>}
+            <div style={{ height: "100vh" }} className="row d-flex align-items-center justify-content-center">
+                <form className="col-md-4 col-10" >
+                    <div className="form-group ">
+                        <input onBlur={handleBlur} defaultValue="Hello World" className="form-control" type="text" name="name" />
+                    </div>
+                </form>
+            </div>
+        </section>
     );
 };
 
